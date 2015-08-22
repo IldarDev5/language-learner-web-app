e94e445abb70f46a80275b8be77044c8c25709c2
IldarS1995 <sayildars555@mail.ru>
2015-08-22 12:55:29 +0300

    Added a trigger that decrements the lessons_count field of Cluster by 1 when a lesson that belongs to this cluster record is removed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+--Function that add some languages to the Language table if it's empty of records
 create or replace function check_languages_existence()
   RETURNS INTEGER AS'
 BEGIN 
@@ -10,7 +11,8 @@ END;
 
 select check_languages_existence();
 
-
+--Updates fields last_lesson_add_date and lessons_count of a cluster
+--when a new lesson is added
 create or replace function update_last_lesson_add_date()
   returns trigger as'
   begin
@@ -27,3 +29,29 @@ create trigger update_last_lesson_add_date_for_cluster
   on lesson
   for each row
   execute procedure update_last_lesson_add_date();
+
+
+--Updates field lessons_count of a cluster when a lesson is removed
+create or replace function update_lessons_count_when_lesson_deleted()
+  returns trigger as'
+  begin
+    update "cluster"
+      set lessons_count = lessons_count - 1
+      where cluster_id = OLD.cluster_id;
+    return NEW;
+  end;
+' LANGUAGE plpgsql;
+
+drop TRIGGER if exists update_lessons_count_when_lesson_deleted_trigger on lesson;
+create trigger update_lessons_count_when_lesson_deleted_trigger
+  after delete
+  on lesson
+  for each row
+  execute procedure update_lessons_count_when_lesson_deleted();
+
+
+
+
+
+
+
