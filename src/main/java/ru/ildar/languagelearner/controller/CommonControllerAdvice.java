21157a3e40646adcdaff54c7d9cf68a5cc896a18
IldarS1995 <sayildars555@mail.ru>
2015-08-29 13:42:28 +0300

    Correct view of data for non-authenticated users a bit and added caching of this data

--- src/main/java/ru/ildar/languagelearner/controller/CommonControllerAdvice.java
+++ src/main/java/ru/ildar/languagelearner/controller/CommonControllerAdvice.java
@@ -11,7 +11,9 @@ import ru.ildar.languagelearner.service.AppUserService;
 import ru.ildar.languagelearner.service.ClusterService;
 import ru.ildar.languagelearner.service.LessonService;
 
+import javax.servlet.ServletContext;
 import java.security.Principal;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,6 +25,12 @@ public class CommonControllerAdvice
     private AppUserService appUserService;
     @Autowired
     private UserDataMaps userDataMaps;
+
+    @Autowired
+    private ServletContext servletContext;
+
+    /** How many minutes to wait before updating application data for non-authenticated users. */
+    private static final double MINUTES_TO_WAIT = 60.0;
 
     @InitBinder
     public void initBinder(WebDataBinder binder)
@@ -41,15 +49,40 @@ public class CommonControllerAdvice
         return null;
     }
 
+    /** Data for non-authenticated users. This data is not so important to be constantly precise,
+     * so we're caching it - the users are shown the same data for amount of time, specified by the
+     * MINUTES_TO_WAIT constant, and after this time is up the update happens, so users can see new
+     * data. */
     @ModelAttribute("notAuthUserData")
+    @SuppressWarnings("unchecked")
     public Map<String, Object> dataForNotAuthUsers(Principal principal)
     {
-        if(principal == null)
+        if(principal != null)
+            return null;
+
+        Date prevUpdateDate = (Date)servletContext.getAttribute("prevUpdateDate");
+        Map<String, Object> map;
+        if(prevUpdateDate == null || getDiffMinutes(new Date(), prevUpdateDate) >= MINUTES_TO_WAIT)
+            //The data is needed to be updated
         {
-            return userDataMaps.getNonAuthUserData();
+            map = userDataMaps.getNonAuthUserData();
+            servletContext.setAttribute("nonAuthUserData", map);
+            servletContext.setAttribute("prevUpdateDate", new Date());
+        }
+        else
+            //Returning the old data as the time is not up yet
+        {
+            map = (Map<String, Object>)servletContext.getAttribute("nonAuthUserData");
         }
 
-        return null;
+        return map;
+    }
+
+    /** Returns difference in minutes between the first and the second dates. */
+    private double getDiffMinutes(Date date, Date prevUpdateDate)
+    {
+        long msDiff = date.getTime() - prevUpdateDate.getTime();
+        return (double)msDiff / 1000 / 60;
     }
 
     @ModelAttribute("authUserData")
