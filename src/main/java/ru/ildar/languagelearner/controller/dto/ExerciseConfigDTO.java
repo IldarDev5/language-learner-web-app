4d8a4ef1860f4b442e16686b5ec0362c019e72c5
IldarS1995 <sayildars555@mail.ru>
2015-08-30 16:34:05 +0300

    Added a function for viewing the whole list of translations of a lesson before starting an exercise with this lesson

--- src/main/java/ru/ildar/languagelearner/controller/dto/ExerciseConfigDTO.java
+++ src/main/java/ru/ildar/languagelearner/controller/dto/ExerciseConfigDTO.java
@@ -4,13 +4,11 @@ import java.io.Serializable;
 
 public class ExerciseConfigDTO implements Serializable
 {
-    /** Count of questions to have in the test */
     private int countOfQuestions;
-    /** Should all questions be unique or there should be also duplicates.
-     * If true, the count of questions is equal to the count of translations in the lesson. */
     private boolean uniqueQuestions;
-    /** Should primary language (that, sentences in which are to be translated) be the second one */
     private boolean invertLanguages;
+    private boolean showLessonBeforeExercise;
+    //TODO: See if everything will be ok without setters
 
     /** Count of questions to have in the test */
     public int getCountOfQuestions()
@@ -44,5 +42,16 @@ public class ExerciseConfigDTO implements Serializable
     public void setInvertLanguages(boolean invertLanguages)
     {
         this.invertLanguages = invertLanguages;
+    }
+
+    /** Show the list of sentence pairs of the lesson before starting the exercise */
+    public boolean isShowLessonBeforeExercise()
+    {
+        return showLessonBeforeExercise;
+    }
+
+    public void setShowLessonBeforeExercise(boolean showLessonBeforeExercise)
+    {
+        this.showLessonBeforeExercise = showLessonBeforeExercise;
     }
 }
