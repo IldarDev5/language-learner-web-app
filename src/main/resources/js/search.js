db3541e39bedfad988e5acc3cfcd0a207ef951d9
IldarS1995 <sayildars555@mail.ru>
2015-09-03 15:42:27 +0300

    Added some styles for search page and started writing JS code

--- src/main/resources/js/search.js
+++ src/main/resources/js/search.js
@@ -1,3 +1,16 @@
 /**
  * Created by Ildar on 01.09.2015.
  */
+
+function searchLessons(caller) {
+    toggleNavTabSelection(caller);
+}
+
+function searchTranslations(caller) {
+    toggleNavTabSelection(caller);
+}
+
+function toggleNavTabSelection(caller) {
+    $(caller).closest("ul").find("li[class='active']").removeAttr("class");
+    $(caller).closest("li").attr("class", "active");
+}
